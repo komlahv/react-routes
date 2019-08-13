8bafaa8c3333cc235002fa9d817f82457d3e0004
VictorEKomlah <komlahv@gmail.com>
2019-08-13 16:59:33 +0000

    routes done

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/topics">Topics</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Header;
